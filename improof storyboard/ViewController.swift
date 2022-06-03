6a86ba16c8d9b328dc5a52d88c5756b266ab6816
wonka <kayuwong05@gmail.com>
2022-06-03 18:55:07 +0800

    Initial Commit

---
+++ improof storyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  improof storyboard
+//
+//  Created by Wong Ka Yu on 2/6/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
